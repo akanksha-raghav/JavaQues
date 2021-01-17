0c92129fb19d12cd7d60b4ac0ab19d2bc63d48fe
Akanksha Raghav <bitturaghav919@gmail.com>
2021-01-17 18:47:49 +0530

    Add files via upload

---
+++ DataTypeCasting.java
@@ -0,0 +1,12 @@
+public class DataTypeCasting{
+public static void main(String [] args){
+// Automatic
+byte x = 4;
+int y = x;
+System.out.println(y);
+// Manual
+double a = 3.1454;
+int b = (int)a;
+System.out.println(b);
+}
+}
