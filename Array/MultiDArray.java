e44e15340aab2c9acb0553490bc6b47132da91d4
Akanksha Raghav <bitturaghav919@gmail.com>
2021-01-19 15:03:10 +0530

    Add files via upload

---
+++ Array/MultiDArray.java
@@ -0,0 +1,13 @@
+public class MultiDArray{
+    public static void main(String[] args) {
+        int a[][] = new int [5][3];
+        System.out.println(a[2][1]);
+        int b[][] = {
+            {1,3,8,5},
+            {2,5,4},
+            {9,8,3,4,1}
+        };
+        System.out.println(b[1][1]);
+
+    }
+}
